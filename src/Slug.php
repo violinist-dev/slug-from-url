a870698762fbd966f1d98a8bcdd1042369ab74f3
Eirik S. Morland <eirik@morland.no>
2018-08-04 16:39:29 +0200

    Fix typo in function name

--- src/Slug.php
+++ src/Slug.php
@@ -164,7 +164,7 @@ class Slug
         return $slug;
     }
 
-  public static function createFromUrAndSupportedProvidersl($url, $supported_providers)
+  public static function createFromUrlAndSupportedProvidersl($url, $supported_providers)
   {
     $slug = new self();
     $slug->setSupportedProviders($supported_providers);
