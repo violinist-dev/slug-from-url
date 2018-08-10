9b73b9b9f130728e3dee479f094c849b9eb2cbb2
Eirik S. Morland <eirik@morland.no>
2018-08-10 08:44:21 +0200

    Add more tests

--- tests/SlugTest.php
+++ tests/SlugTest.php
@@ -10,11 +10,7 @@ class SlugTest extends TestCase
     public function testCanCreateStatically()
     {
         $slug = Slug::createFromUrl('https://github.com/violinist-dev/slug-from-url');
-        $this->assertEquals('slug-from-url', $slug->getUserRepo());
-        $this->assertEquals('github.com', $slug->getProvider());
-        $this->assertEquals('violinist-dev/slug-from-url', $slug->getSlug());
-        $this->assertEquals('violinist-dev', $slug->getUserName());
-        $this->assertEquals('https://github.com/violinist-dev/slug-from-url', $slug->getUrl());
+        $this->checkExpectedOutputFromSelfRepo($slug);
     }
 
     public function testNonSupportedProvider()
@@ -24,5 +20,29 @@ class SlugTest extends TestCase
         $this->expectExceptionMessage('No username found');
         $slug->getUserName();
         $this->assertEquals(null, $slug->getProvider());
+    }
+
+    public function testcreateStaticallyFromSupportedProviders()
+    {
+        $slug = Slug::createFromUrlAndSupportedProviders('https://example.com/violinist-dev/slug-from-url', ['example.com']);
+        $this->checkExpectedOutputFromSelfRepo($slug, 'example.com');
+    }
+
+    public function testcreateStaticallyFromUnSupportedProviders()
+    {
+        $slug = Slug::createFromUrlAndSupportedProviders('https://example.com/violinist-dev/slug-from-url', ['example.org']);
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('No username found');
+        $slug->getUserName();
+        $this->assertEquals(null, $slug->getProvider());
+    }
+
+    protected function checkExpectedOutputFromSelfRepo(Slug $slug, $host = 'github.com')
+    {
+        $this->assertEquals('slug-from-url', $slug->getUserRepo());
+        $this->assertEquals($host, $slug->getProvider());
+        $this->assertEquals('violinist-dev/slug-from-url', $slug->getSlug());
+        $this->assertEquals('violinist-dev', $slug->getUserName());
+        $this->assertEquals("https://$host/violinist-dev/slug-from-url", $slug->getUrl());
     }
 }
